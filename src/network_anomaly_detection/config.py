d5d34193fd8f7cfa7e374dd62ff5dd5ab532dc77
rky-w <57967688+rky-w@users.noreply.github.com>
2026-02-09 01:11:26 +0000

    Adding config

---
+++ src/network_anomaly_detection/config.py
@@ -0,0 +1,13 @@
+
+
+config = {
+    "persist_window": 5,
+    "persist_threshold": 5,
+    "seasonal_freq": 288,
+    "seasonal_threshold": 4,
+    "level_shift_window": 10,
+    "level_shift_threshold": 4,
+    "pelt_penalty": 20,
+    "pelt_seasonal_decomposition": True,
+    "isolation_forest_contamination": 0.01
+}
